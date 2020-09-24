1801515585eeba6df3898647f0db4b64bf42b535
ratroot92 <zcomisb.007@gmail.com>
2020-09-24 16:20:49 +0500

    asd

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express')
 var cors = require('cors')
-const path=require('path')
+const path = require('path')
 const app = express()
 
 app.use(cors())
 
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/AnyChart.html'));
 });
-app.get('/barchart', function(req, res) {
+app.get('/barchart', function (req, res) {
   res.sendFile(path.join(__dirname + '/BarChart.html'));
 });
-app.get('/highchart', function(req, res) {
+app.get('/highchart', function (req, res) {
   res.sendFile(path.join(__dirname + '/HighChart2.html'));
 });
 
@@ -25,7 +25,8 @@ app.get('/api', (req, res) => {
       public: "8.9",
       philanthropic: "829",
       commercial: "0.1",
-      total: "100"
+      total: "100",
+      color:"#6279c5"
     },
     {
       name: "Russia",
@@ -57,7 +58,8 @@ app.get('/api', (req, res) => {
       public: "8.9",
       philanthropic: "829",
       commercial: "0.1",
-      total: "350"
+      total: "350",
+      color:"#b37faf"
     },
     {
       name: "Netherlands",
@@ -65,7 +67,9 @@ app.get('/api', (req, res) => {
       public: "8.9",
       philanthropic: "829",
       commercial: "0.1",
-      total: "450"
+      total: "450",
+      color:"#95004f"
+
     },
     {
       name: "India",
@@ -73,7 +77,8 @@ app.get('/api', (req, res) => {
       public: "8.9",
       philanthropic: "829",
       commercial: "0.1",
-      total: "560"
+      total: "560",
+      color: "#b4de00",
     },
     {
       name: "Italy",
@@ -160,84 +165,142 @@ app.get('/api', (req, res) => {
 
 
 app.get('/api2', (req, res) => {
- var Data=[
-    
+  var Data = [
+
     {
-    code3: "AFG",
-    name: "Afghanistan",
-    public: "8.9",
-      philanthropic: "829",
-      commercial: "0.1",
-      total: "250",
-    code: "AF"
+      code3: "AFG",
+      name: "Afghanistan",
+      public: 350,
+      philanthropic: 150,
+      commercial: 250,
+      total: 750,
+      code: "AF",
+      color: "#ffce0a"
     },
     {
-      code3: "CHN",
-      name: "China",
-
-      public: "8.9",
-        philanthropic: "829",
-        commercial: "0.1",
-        total: "250",
-      code: "CN"
-      }
+      code3: "IND",
+      name: "India",
+      public: 400,
+      philanthropic: 800,
+      commercial: 100,
+      total: 1300,
+      code: "IN",
+      color: "#b4de00",
+    }
     ,
     {
       code3: "RUS",
       name: "Russian Federation",
-      public: "8.9",
-        philanthropic: "829",
-        commercial: "0.1",
-        total: "250",
-        code: "RU"
-      },
-      {
-        code3: "USA",
-        name: "United States",
-        public: "8.9",
-          philanthropic: "829",
-          commercial: "0.1",
-          total: "250",
-          code: "US"
-        },
+      public: 100,
+      philanthropic: 150,
+      commercial: 200,
+      total: 450,
+      code: "RU",
+      color: "#ff0000"
+    },
+    {
+      code3: "USA",
+      name: "United States",
+      public: 50,
+      philanthropic: 100,
+      commercial: 25,
+      total: 175,
+      code: "US",
+      color:"#6279c5"
+    },
 
-        {
-          code3: "ZAF",
-          name: "South Africa",
-          public: "8.9",
-            philanthropic: "829",
-            commercial: "0.1",
-            total: "250",
-            code: "ZA"
-          },
+    {
+      code3: "ZAF",
+      name: "South Africa",
+      public: 20,
+      philanthropic: 30,
+      commercial: 10,
+      total: 60,
+      code: "ZA",
+      color:"#b37faf"
+    
+    },
 
-
-          {
-            code3: "IND",
-            name: "India",
-            public: "8.9",
-              philanthropic: "829",
-              commercial: "0.1",
-              total: "250",
-              code: "IN"
-            },
-
-
-            {
-              code3: "DEU",
-              name: "Germany",
-              public: "8.9",
-                philanthropic: "829",
-                commercial: "0.1",
-                total: "250",
-                code: "DE"
-              },
-      ]
-    res.json(Data)
-  })
+    {
+      code3: "NLD",
+      name: "Netherlands",
+      public: 20,
+      philanthropic: 30,
+      commercial: 10,
+      total: 60,
+      code: "Nl",
+      color:"#95004f"
+    
+    },
 
 
+    {
+      code3: "POL",
+      name: "Poland",
+      public: 20,
+      philanthropic: 30,
+      commercial: 10,
+      total: 60,
+      code: "PL",
+      color:"#95004f"
+    
+    },
 
-  const port = process.env.PORT || 3000;
 
-  app.listen(port, () => console.log(`Listening on ${port}`));
+    {
+      code3: "FRA",
+      name: "France",
+      public: 20,
+      philanthropic: 30,
+      commercial: 10,
+      total: 60,
+      code: "FR",
+      color:"#95004f"
+    
+    },
+
+
+    {
+      code3: "DEU",
+      name: "Germany",
+      public: 20,
+      philanthropic: 30,
+      commercial: 10,
+      total: 60,
+      code: "DE",
+      color:"#95004f"
+    
+    },
+
+
+    {
+      code3: "AUS",
+      name: "Australia",
+      public: 10,
+      philanthropic: 40,
+      commercial: 90,
+      total: 140,
+      code: "AU",
+      color:"#f57b0d",
+    },
+
+
+    {
+      code3: "BRA",
+      name: "Brazil",
+      public: 125,
+      philanthropic: 50,
+      commercial: 0,
+      total: 175,
+      code: "BR",
+      color:"#37cc9a",
+    },
+  ]
+  res.json(Data)
+})
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on ${port}`));
